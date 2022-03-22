d7d04fc8a5425bac2a424273e64672baed07a081
birbe <birb.seeb@gmail.com>
2022-03-22 15:41:24 +0100

    Update build script to work with Windows maybe

--- rust/wgpu-mc-demo/build.rs
+++ rust/wgpu-mc-demo/build.rs
@@ -15,10 +15,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let resources_root: std::path::PathBuf = "./res/assets".into();
     if !resources_root.is_dir() {
-        let path = std::path::PathBuf::from("/tmp/mc-jar-cache");
+        let path = std::path::PathBuf::from("./tmp/mc-jar-cache");
 
         if !path.is_dir() {
-            std::fs::create_dir("/tmp/mc-jar-cache")?;
+            std::fs::create_dir("./tmp/mc-jar-cache")?;
         }
 
         let response = reqwest::blocking::get("https://launcher.mojang.com/v1/objects/37fd3c903861eeff3bc24b71eed48f828b5269c8/client.jar").unwrap();
@@ -28,6 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         fs_extra::dir::copy(path.join("assets"), "./res/", &copy_options)?;
 
         std::fs::remove_dir_all(path)?;
+        std::fs::remove_dir_all("./tmp/mc-jar-cache")?;
     }
 
     Ok(())
