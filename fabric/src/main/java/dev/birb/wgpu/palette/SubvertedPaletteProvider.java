ab4cffc2a902ec1e92a62f37d63d22c274267747
Birbe <birb.seeb@gmail.com>
2022-04-23 10:55:05 +0200

    make the game launch

--- fabric/src/main/java/dev/birb/wgpu/palette/SubvertedPaletteProvider.java
+++ fabric/src/main/java/dev/birb/wgpu/palette/SubvertedPaletteProvider.java
@@ -5,7 +5,6 @@ import net.minecraft.util.collection.IndexedIterable;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.chunk.IdListPalette;
 import net.minecraft.world.chunk.Palette;
-import net.minecraft.world.chunk.PaletteResizeListener;
 import net.minecraft.world.chunk.PalettedContainer;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,12 +17,7 @@ public class SubvertedPaletteProvider {
 
         @Override
         public <A> PalettedContainer.DataProvider<A> createDataProvider(IndexedIterable<A> idList, int bits) {
-            return new PalettedContainer.DataProvider<>(new Palette.Factory() {
-                @Override
-                public <A> Palette<A> create(int bits, IndexedIterable<A> idList, PaletteResizeListener<A> listener, List<A> list) {
-                    return new RustPalette<>(WgpuNative.uploadIdList(idList));
-                }
-            }, bits);
+            return new PalettedContainer.DataProvider<>(BI_MAP, bits);
         }
     };
 
