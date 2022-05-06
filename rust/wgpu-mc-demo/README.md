2c80909e703d0a2070fcc97ae14e9f1d4439e0ca
Simon Laux <mobile.info@simonlaux.de>
2022-05-06 18:40:42 +0200

    improve and fix demo build script

--- rust/wgpu-mc-demo/README.md
+++ rust/wgpu-mc-demo/README.md
@@ -1,3 +1,8 @@
 # Demo Crate
 
 Demonstrates the features of the engine, separately from any actual game logic.
+
+## How to run the demo
+
+1. make sure your working directory is this folder. (the build script needs it that way)
+2. run `cargo +nightly run`
--- rust/wgpu-mc-demo/build.rs
+++ rust/wgpu-mc-demo/build.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::io;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = env::var("OUT_DIR")?;
@@ -7,35 +8,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut copy_options = fs_extra::dir::CopyOptions::new();
     copy_options.overwrite = true;
 
-    let mut paths_to_copy = Vec::new();
-    paths_to_copy.push("./res/");
+    let paths_to_copy: Vec<&str> = vec!["./res/"];
     fs_extra::copy_items(&paths_to_copy, out_dir, &copy_options)?;
 
-    let path = std::path::PathBuf::from("./res/assets");
+    let resources_root: PathBuf = "./res/assets".into();
+    let resources_root_tmp: PathBuf = "./res/assets_tmp".into();
+    let temp_path = PathBuf::from("./tmp/mc-jar-cache");
 
-    if !path.is_dir() {
-        std::fs::create_dir("./res/assets")?;
-    }
-
-    fs_extra::dir::copy("./res/wgpu_mc", "./res/assets", &copy_options);
-
-    let resources_root: std::path::PathBuf = "./res/assets".into();
     if !resources_root.is_dir() {
-        let path = std::path::PathBuf::from("./tmp/mc-jar-cache");
+        println!(
+            "building assets, because {:?} does not exist yet",
+            resources_root
+        );
+        fs_extra::dir::create_all(&resources_root_tmp, true)?;
 
-        if !path.is_dir() {
-            std::fs::create_dir("./tmp/mc-jar-cache")?;
+        if !temp_path.is_dir() {
+            fs_extra::dir::create_all(&temp_path, true)
+                .expect("tmp dir for downloading client.jar data");
         }
-
+        println!("download offical mc client data");
         let response = reqwest::blocking::get("https://launcher.mojang.com/v1/objects/37fd3c903861eeff3bc24b71eed48f828b5269c8/client.jar").unwrap();
         let content = io::Cursor::new(response.bytes()?);
+        println!("unpacking offical mc client data");
         let mut zip = zip::read::ZipArchive::new(content)?;
-        zip.extract(&path)?;
-        fs_extra::dir::copy(path.join("assets"), "./res/", &copy_options)?;
+        zip.extract(&temp_path)?;
 
-        std::fs::remove_dir_all(path)?;
-        std::fs::remove_dir_all("./tmp/mc-jar-cache")?;
+        let mut copy_content_only = fs_extra::dir::CopyOptions::new();
+        copy_content_only.content_only = true;
+        copy_content_only.overwrite = true;
+
+        fs_extra::dir::copy(
+            temp_path.join("assets"),
+            &resources_root_tmp,
+            &copy_content_only,
+        )?;
+        std::fs::remove_dir_all(temp_path)?;
+
+        fs_extra::dir::move_dir(&resources_root_tmp, &resources_root, &copy_content_only)?;
     }
+
+    println!("copy shader source code");
+    fs_extra::dir::copy("./res/wgpu_mc", &resources_root, &copy_options)?;
 
     Ok(())
 }
