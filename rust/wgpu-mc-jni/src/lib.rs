66c3372399aa4d4871437eb27679b3849468a6df
Zanctarian <44514121+Zanctarian@users.noreply.github.com>
2023-01-14 06:56:03 -0600

    whoops

--- rust/wgpu-mc-jni/src/lib.rs
+++ rust/wgpu-mc-jni/src/lib.rs
@@ -154,11 +154,11 @@ impl<'a> BlockStateProvider for MinecraftBlockstateProvider<'a> {
         let mut chunk_z = (z / 16) - self.pos[1];
 
         //Rust i32 doesn't properly floor negative numbers, so here's a dirty fix.
-        if x < 16 && x % 16 != 0 {
+        if x < 0 && x % 16 != 0 {
             chunk_x -= 1;
         }
 
-        if z < 16 && z % 16 != 0 {
+        if z < 0 && z % 16 != 0 {
             chunk_z -= 1;
         }
 
