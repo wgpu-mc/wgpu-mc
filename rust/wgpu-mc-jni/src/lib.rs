a6d6a2118f2b514c7486aecf1cb359617015d1d6
Zanctarian <44514121+Zanctarian@users.noreply.github.com>
2023-12-31 08:55:28 -0600

    Fix title screen panorama (#100)

--- rust/wgpu-mc-jni/src/lib.rs
+++ rust/wgpu-mc-jni/src/lib.rs
@@ -1107,7 +1107,7 @@ pub fn setProjectionMatrix(mut env: JNIEnv, _class: JClass, float_array: JFloatA
 
     let slice_4x4: [[f32; 4]; 4] = *bytemuck::from_bytes(bytemuck::cast_slice(&converted));
 
-    let matrix = Matrix4::from(slice_4x4) * Matrix4::from_nonuniform_scale(1.0, 1.0, 0.0);
+    let matrix = Matrix4::from(slice_4x4);
 
     GL_COMMANDS.write().0.push(GLCommand::SetMatrix(matrix));
 }
